da3e31ea37a9868b3bcf4a28a3a17e82149f462d
ILoID <enric.carstens@gmail.com>
2020-10-08 01:20:36 +0200

    Update FileUpdater.py

--- FileUpdater.py
+++ FileUpdater.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import os
 import time
 
+
+# Enter your directory names here
 SOURCE_DIR_NAME = 'Source'
 TARGET_DIR_NAME = 'Target'
 SUB_DIR_1 = '1'
@@ -11,13 +13,13 @@ SUB_DIR_3 = '3'
 SUB_DIR_4 = '4'
 SUB_DIR_5 = '5'
 
-base_path = Path.cwd()
 
+base_path = Path.cwd()
 source_path = base_path.joinpath(SOURCE_DIR_NAME)
 source_files = os.listdir(source_path)
-
 target_dir = base_path.joinpath(TARGET_DIR_NAME)
 target_paths = [target_dir.joinpath(SUB_DIR_1), target_dir.joinpath(SUB_DIR_2), target_dir.joinpath(SUB_DIR_3), target_dir.joinpath(SUB_DIR_4), target_dir.joinpath(SUB_DIR_5)]
+
 
 def update_files():
     for target_path in target_paths:
